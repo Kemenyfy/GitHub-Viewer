b4b021630c11835d54175b83878d2b1d052cf08b
Gus Kemenyfy <gustavo.kemenyfy@gmail.com>
2019-01-21 15:13:29 -0500

    Search Users Working and Displaying. Added Table for Repos, Stars and Language.

--- Github Viewer/app/controllers/MainController.js
+++ Github Viewer/app/controllers/MainController.js
@@ -1,3 +1,25 @@
-﻿app.controller("MainController", function ($scope) {
-    $scope.Message = "Hello To GitHub Viewer";
+﻿app.controller("MainController", function ($scope, $http) {
+
+    var onUserComplete = function (response) {
+        $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, onError);
+    };
+
+    var onRepos = function (response) {
+        $scope.repos = response.data;
+    };
+
+    var onError = function (reason) {
+        $scope.error = "Could not Fetch the User";
+    };
+
+    $scope.search = function (username) {
+        $http.get("https://api.github.com/users/" + username)
+            .then(onUserComplete, onError);
+    };
+
+    $scope.username = "";
+    $scope.Message = "Github Viewer";
+    
 });
